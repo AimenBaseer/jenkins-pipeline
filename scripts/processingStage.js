efdda2a20fce39a689dc42cc836052fbc757dd30
AimenBaseer <aimenbaseer@gmail.com>
2022-06-19 15:33:40 +0500

    chore: updated processed data

--- scripts/processingStage.js
+++ scripts/processingStage.js
@@ -1,9 +1,12 @@
 const fs = require("fs");
+const _= require('lodash')
 
 const processData = () => {
   const dir = `${__dirname}/data/`;
   const data = fs.readFileSync(`${dir}/pipelineContent.txt`, "utf8");
-  const processedData= `length of content in file: ${data.length}`
+  const processedData= `length of content in file: ${_.map(data,(value)=>(value))}`;
+  console.log(processedData)
+  
   fs.writeFileSync(`${dir}/pipelineContent.txt`,processedData);
 };
 
